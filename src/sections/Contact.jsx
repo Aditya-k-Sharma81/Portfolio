cf43d8c1b83eb93fccc4a279e4dbf158025e6683
Aditya Kumar Sharma <adityasharmaas813@gmail.com>
2026-03-15 23:22:35 +0530

    connect with backend

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -26,7 +26,7 @@ const Contact = () => {
     if (validate()) {
       setIsSubmitting(true);
       try {
-        const response = await fetch('http://localhost:5000/api/send-email', {
+        const response = await fetch('https://portfolio-5xcp.onrender.com/api/send-email', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
